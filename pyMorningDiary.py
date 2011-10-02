56da777f2403c59dd532d61f7e48d9dbdf03493f
Felix Lu <lufelixgh@gmail.com>
2011-10-02 22:53:06 +0800

    UI Demo.

--- pyMorningDiary.py
+++ pyMorningDiary.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from tkinter import Tk, Frame, StringVar, Label, Text, Entry, LEFT, YES, BOTH
+from tkinter import Tk, Frame, StringVar, Label, Text, Entry, LEFT, YES, \
+    BOTH, END, Button
 
 
 class Diary(Frame):
@@ -14,7 +15,11 @@ class Diary(Frame):
         self.create_widgets()
 
     def create_widgets(self):
-        DiaryCell(self.frame, TITLE_1x1, 'blue').grid(row=1, column=0)
+        Button(self.frame, text='say "Hello"', command=self.say_hello).grid(row=0, column=0)
+        Button(self.frame, text='say "你好"', command=self.say_hello_chs).grid(row=0, column=1)
+        Button(self.frame, text='Print', command=self.prt).grid(row=0, column=2)
+        self.dc1 = DiaryCell(self.frame, TITLE_1x1, 'blue')
+        self.dc1.grid(row=1, column=0)
         DiaryCell(self.frame, TITLE_1x2, 'green').grid(row=1, column=1)
         DiaryCell(self.frame, TITLE_1x3, 'red').grid(row=1, column=2)
         DiaryCell(self.frame, TITLE_2x1, 'yellow').grid(row=2, column=0)
@@ -23,6 +28,15 @@ class Diary(Frame):
         DiaryCell(self.frame, TITLE_3x1, 'green').grid(row=3, column=0)
         DiaryCell(self.frame, TITLE_3x2, 'red').grid(row=3, column=1)
         DiaryCell(self.frame, TITLE_3x3, 'yellow').grid(row=3, column=2)
+
+    def say_hello(self):
+        self.dc1.set_text('Hello')
+
+    def say_hello_chs(self):
+        self.dc1.set_text('你好')
+
+    def prt(self):
+        print(self.dc1.get_text())
 
 
 class DiaryCell(Frame):
@@ -42,13 +56,19 @@ class DiaryCell(Frame):
         self.txt_cell = Text(self.frame, height=12, width=35, bd=3,
             bg=self.bg_color)
         self.txt_cell.grid(row=1, column=0, sticky='EW')
-        #self.txt_cell.insert(END, '测试文本')
 
-    def set_text(self, str_text):
-        pass
+    def get_title(self):
+        return self.title_var.get()
+
+    def set_title(self, str_title):
+        self.title_var.set(str_title)
 
     def get_text(self):
-        pass
+        return self.txt_cell.get(0.0, END)
+
+    def set_text(self, str_text):
+        self.txt_cell.delete(0.0, END)
+        self.txt_cell.insert(END, str_text)
 
 
 class DiaryMeta(Frame):
@@ -105,14 +125,23 @@ class DiaryMeta(Frame):
         Entry(self.frame, textvariable=self.getup_var).grid(row=8, column=1)
 
 
-TITLE_1x1 = '工作'
-TITLE_1x2 = '人生梦想'
-TITLE_1x3 = '家庭'
-TITLE_2x1 = '金钱'
-TITLE_2x3 = '健康'
-TITLE_3x1 = '爱好'
-TITLE_3x2 = '人际关系'
-TITLE_3x3 = '其他'
+class Calendar(Frame):
+
+    def __init__(self, parent):
+        Frame.__init__(self, parent)
+        self.frame = Frame(self)
+        self.frame.pack(side=LEFT, expand=YES, fill=BOTH)
+
+
+
+TITLE_1x1 = '人生梦想'
+TITLE_1x2 = '工作、同事'
+TITLE_1x3 = '家庭、亲戚'
+TITLE_2x1 = '金钱、理财'
+TITLE_2x3 = '健康、运动'
+TITLE_3x1 = '兴趣、爱好'
+TITLE_3x2 = '朋友、同学'
+TITLE_3x3 = '娱乐、其他'
 
 WEATHER = '天气'
 TEMPERATURE = '温度'
